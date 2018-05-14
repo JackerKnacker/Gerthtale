4a44291b3632795de2de483f7854e048d79c58d0
Jakir Ansari <36454956+JackerKnacker@users.noreply.github.com>
2018-05-13 23:24:14 -0400

    Add files via upload

--- Moving.java
+++ Moving.java
@@ -46,22 +46,29 @@ class GamePanel extends JPanel implements KeyListener {
 	private boolean[] keys;
 	private int mapx, mapy;
 	private double playerFrame;
-	Image map = new ImageIcon("map1.png").getImage();
+	private String lastDirection = "down";
+	Image map = new ImageIcon("src\\Gerthtale\\map1.png").getImage();
+	//Image mask = new ImageIcon("src\\Gerthtale\\map1mask.png").getImage();;
 
 	//Player sprites
-	Image[] playerDown;
-	Image[] playerUp;
-	Image[] playerLeft;
-	Image[] playerRight;
+	Image[] playerDown, playerUp, playerLeft, playerRight;
 
 	public GamePanel() {
 		keys = new boolean[KeyEvent.KEY_LAST + 1];
-		playerDown = new Image[8];
+		playerDown = new Image[4];
+		playerUp = new Image[4];
+		playerLeft = new Image[4];
+		playerRight = new Image[4];
 		for (int i = 0; i < 4; i++) {
-			playerDown[i] = new ImageIcon("char1sprites/char1-" + (i+1) + ".png").getImage().getScaledInstance(48, 64,
+			playerDown[i] = new ImageIcon("src\\Gerthtale\\char1sprites\\char1-" + (i+1) + ".png").getImage().getScaledInstance(48, 64,
+					Image.SCALE_DEFAULT);
+			playerLeft[i] = new ImageIcon("src\\Gerthtale\\char1sprites\\char1-" + (i+5) + ".png").getImage().getScaledInstance(48, 64,
+					Image.SCALE_DEFAULT);
+			playerRight[i] = new ImageIcon("src\\Gerthtale\\char1sprites\\char1-" + (i+9) + ".png").getImage().getScaledInstance(48, 64,
+					Image.SCALE_DEFAULT);
+			playerUp[i] = new ImageIcon("src\\Gerthtale\\char1sprites\\char1-" + (i+13) + ".png").getImage().getScaledInstance(48, 64,
 					Image.SCALE_DEFAULT);
 		}
-
 		mapx = 256;
 		mapy = 256;
 		playerFrame = 0;
@@ -79,29 +86,40 @@ class GamePanel extends JPanel implements KeyListener {
 	}
 	public void keyReleased(KeyEvent e) {
 		keys[e.getKeyCode()] = false;
-		playerFrame = 0;
+		playerFrame = 0; //Restarts frame if user stops pressing key and starts again
 	}
 	public void move() {
 		requestFocus();
 		if (keys[KeyEvent.VK_LEFT]) {
+			lastDirection = "left";
+			playerFrame += 0.2;
+			if (playerFrame > 3.8)
+				playerFrame = 0;
 			if (mapx < 376)
 				mapx += 4;
 		}
-		if (keys[KeyEvent.VK_RIGHT]) {
-			if (mapx+1024 > 376+64) //if mapx + map width > 368 + player sprite width
+		else if (keys[KeyEvent.VK_RIGHT]) {
+			lastDirection = "right";
+			playerFrame += 0.2;
+			if (playerFrame > 3.8)
+				playerFrame = 0;
+			if (mapx+1024 > 376+48) //if mapx + map width > 376 + player sprite width
 				mapx -= 4;
 		}
-		if (keys[KeyEvent.VK_DOWN]) {
+		else if (keys[KeyEvent.VK_DOWN]) {
+			lastDirection = "down";
+			playerFrame += 0.2;
+			if (playerFrame > 3.8) 
+				playerFrame = 0;
 			if (mapy+768 > 268+64){ //if mapy + map length > 268 + player sprite length
-				playerFrame += 0.15;
-				if (playerFrame >= 3.75) {
-					playerFrame = 0;
-				}
 				mapy -= 4;
 			}
-			//System.out.println(mapx + " " + mapy);
 		}
-		if (keys[KeyEvent.VK_UP]) {
+		else if (keys[KeyEvent.VK_UP]) {
+			lastDirection = "up";
+			playerFrame += 0.2;
+			if (playerFrame > 3.8)
+				playerFrame = 0;
 			if (mapy < 268)
 				mapy += 4;
 		}
@@ -109,13 +127,35 @@ class GamePanel extends JPanel implements KeyListener {
 
 	public void displayPlayer(Graphics g) {
 		requestFocus();
-		g.drawImage(playerDown[(int) Math.floor(playerFrame)], 376, 268, this); //368, 268 = IN THE MIDDLE OF SCREEN
+		if (keys[KeyEvent.VK_LEFT]) {
+			g.drawImage(playerLeft[(int) Math.floor(playerFrame)], 376, 268, this);
+		}
+		else if (keys[KeyEvent.VK_RIGHT]) {
+			g.drawImage(playerRight[(int) Math.floor(playerFrame)], 376, 268, this);
+		}
+		else if (keys[KeyEvent.VK_UP]) {
+			g.drawImage(playerUp[(int) Math.floor(playerFrame)], 376, 268, this);
+		}
+		else if (keys[KeyEvent.VK_DOWN]) {
+			g.drawImage(playerDown[(int) Math.floor(playerFrame)], 376, 268, this); //368, 268 = IN THE MIDDLE OF SCREEN
+		}
+		else { //When player is not moving
+			if (lastDirection.equals("down"))
+				g.drawImage(playerDown[0], 376, 268, this);
+			else if (lastDirection.equals("up"))
+				g.drawImage(playerUp[0], 376, 268, this);
+			else if (lastDirection.equals("left"))
+				g.drawImage(playerLeft[0], 376, 268, this);
+			else if (lastDirection.equals("right"))
+				g.drawImage(playerRight[0], 376, 268, this);
+		}
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		//g.drawImage(mask, mapx, mapy, this);
 		g.drawImage(map, mapx, mapy, this);
 		displayPlayer(g);
 	}
